f04ed609b992355b129c5f296fe21870f9179cf3
leeys9423 <leeys9423@gmail.com>
2024-12-18 11:57:06 +0900

    feat: security.properties -> .env 환경변수 파일 교체

--- backend/src/main/java/com/redbox/RedboxApplication.java
+++ backend/src/main/java/com/redbox/RedboxApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:security.properties")
+@PropertySource("classpath:.env")
 public class RedboxApplication {
 
     public static void main(String[] args) {
