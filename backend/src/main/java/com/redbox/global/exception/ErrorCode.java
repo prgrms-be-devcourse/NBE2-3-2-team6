9c9fdad37648d32901dba99555c9768b6a8ea1f3
leeys9423 <leeys9423@gmail.com>
2024-12-18 17:51:00 +0900

    feat: 이메일 util 기능 구현 (RB-115)

--- backend/src/main/java/com/redbox/global/exception/ErrorCode.java
+++ backend/src/main/java/com/redbox/global/exception/ErrorCode.java
@@ -7,8 +7,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    ;
+    // Email 관련 에러
+    EMAIL_CREATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이메일 메시지 생성 중 오류가 발생했습니다."),
+    EMAIL_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이메일 전송 중 오류가 발생했습니다.");
 
-    private final String message;
     private final HttpStatus status;
+    private final String message;
 }
