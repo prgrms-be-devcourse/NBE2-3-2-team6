3b14d087192fa2a4251d2c6a9b168257492a3310
Jhynguu <jhyngu0531@gmail.com>
2024-12-23 01:27:25 +0900

    config: SecurityConfig 구현 (RB-138)

--- backend/src/main/java/com/redbox/global/config/SecurityConfig.java
+++ backend/src/main/java/com/redbox/global/config/SecurityConfig.java
@@ -1,13 +1,22 @@
 package com.redbox.global.config;
 
+import com.redbox.domain.auth.filter.CustomLogoutFilter;
+import com.redbox.domain.auth.filter.JWTFilter;
+import com.redbox.domain.auth.util.JWTUtil;
+import com.redbox.domain.auth.filter.LoginFilter;
+import com.redbox.domain.auth.repository.RefreshRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -18,6 +27,25 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
+    private final AuthenticationConfiguration authenticationConfiguration;
+    // JWTUtil 주입
+    private final JWTUtil jwtUtil;
+    private final RefreshRepository refreshRepository;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil, RefreshRepository refreshRepository) {
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.jwtUtil = jwtUtil;
+        this.refreshRepository = refreshRepository;
+    }
+
+    //AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -25,17 +53,35 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf(AbstractHttpConfigurer::disable) // API 서버이므로
                 .authorizeHttpRequests(auth -> auth
-                    // 이메일 인증 관련 엔드포인트 허용
-                    .requestMatchers("/auth/email/**").permitAll()
-                    // 회원가입, 로그인 관련 엔드포인트 허용
-                    .requestMatchers("/auth/**").permitAll()
-                    .anyRequest().authenticated()
+                        // 이메일 인증 관련 엔드포인트 허용
+                        .requestMatchers("/auth/email/**").permitAll()
+                        // 회원가입, 로그인 관련 엔드포인트 허용
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/reissue").permitAll()
+                        .anyRequest().authenticated()
                 )
+                // Form 로그인 방식 disable
+                .formLogin((auth) -> auth.disable())
+                // 기본 LogoutFilter disable
+                .logout((auth) -> auth.disable())
+                // http basic 인증 방식 disable
+                .httpBasic((auth) -> auth.disable())
+                // JWTFilter 등록
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class)
+                //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+                // AuthenticationManager() JWTUtil 인수 전달
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, refreshRepository), UsernamePasswordAuthenticationFilter.class)
+                // 로그아웃
+                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), UsernamePasswordAuthenticationFilter.class)
+                // 세션 설정
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()));
-        // ... 다른 보안 설정들
+
 
         return http.build();
     }
