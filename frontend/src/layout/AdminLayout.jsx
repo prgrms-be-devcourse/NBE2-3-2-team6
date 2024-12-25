3d356e7f173ca93655391b2a59f842a7d40c3df3
leeys9423 <leeys9423@gmail.com>
2024-12-26 00:22:56 +0900

    feat: 관리자용 헤더 추가

--- frontend/src/layout/AdminLayout.jsx
+++ frontend/src/layout/AdminLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import AdminHeader from "../components/AdminHeader";
 import Footer from "../components/Footer";
 
 export default function AdminLayout() {
   return (
     <div className="flex flex-col min-h-screen">
+      <AdminHeader />
       <Outlet />
       <Footer />
     </div>
