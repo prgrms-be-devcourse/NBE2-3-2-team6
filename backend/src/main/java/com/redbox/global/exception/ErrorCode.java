4afb65e02240b90d7dbf6c14963d54c0440e0f8a
leeys9423 <leeys9423@gmail.com>
2024-12-29 21:29:05 +0900

    fix: 토큰 재발급이 안되는 오류 수정 - JWTFilter에서 401번 오류를 직접 터트림 - 토큰만료되었을때, 401번 오류 코드로 수정

--- backend/src/main/java/com/redbox/global/exception/ErrorCode.java
+++ backend/src/main/java/com/redbox/global/exception/ErrorCode.java
@@ -36,7 +36,7 @@ public enum ErrorCode {
     // 토큰
     TOKEN_NOT_FOUND(HttpStatus.BAD_REQUEST, "Refresh 토큰이 존재하지 않습니다."),
     INVALID_TOKEN(HttpStatus.BAD_REQUEST, "유효하지 않은 토큰입니다."),
-    EXPIRED_TOKEN(HttpStatus.BAD_REQUEST, "토큰이 만료되었습니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
 
     private final HttpStatus status;
