fe67e1fba2222d5ce437d8cfd10a50a56782780d
Bin-Choi <csb5268@gmail.com>
2025-01-04 19:07:17 +0900

    feat: social OAuth2 - OAuth2 설정 - 처음 로그인시 회원 생성

--- backend/src/main/java/com/redbox/global/oauth2/service/CustomOAuth2UserService.java
+++ backend/src/main/java/com/redbox/global/oauth2/service/CustomOAuth2UserService.java
@@ -1,17 +1,26 @@
 package com.redbox.global.oauth2.service;
 
+import com.redbox.domain.user.entity.RoleType;
+import com.redbox.domain.user.entity.Status;
+import com.redbox.domain.user.entity.User;
+import com.redbox.domain.user.repository.UserRepository;
+import com.redbox.global.oauth2.dto.CustomOAuth2User;
 import com.redbox.global.oauth2.dto.NaverResponse;
 import com.redbox.global.oauth2.dto.OAuth2Response;
+import com.redbox.global.oauth2.dto.SocialUserDTO;
 
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.OAuth2RefreshToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {this.userRepository = userRepository;}
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -30,6 +39,27 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         else {
             return null;
         }
-        return oAuth2User;
+
+        String userEmail = oAuth2Response.getEmail();
+        User existData = userRepository.findByEmail(userEmail);
+
+        if (existData == null) {
+            // email 을 가진 유저가 없는 경우 (처음 가입)
+            User newUser = User.builder()
+                               .name(oAuth2Response.getName())
+                               .email(oAuth2Response.getEmail())
+                               .status(Status.ACTIVE)
+                               .roleType(RoleType.USER).build();
+
+            existData = userRepository.save(newUser);
+
+        }
+
+        SocialUserDTO socialUserDTO = new SocialUserDTO();
+        socialUserDTO.setName(existData.getName());
+        socialUserDTO.setEmail(existData.getEmail());
+        socialUserDTO.setRole(existData.getRoleType());
+
+        return new CustomOAuth2User(socialUserDTO);
     }
 }
