cbb652504e1f9edbd689a4a5ba11d96f8e34031e
leeys9423 <leeys9423@gmail.com>
2025-01-06 03:37:30 +0900

    fix: 개인기부 버그 수정

--- frontend/src/pages/DonatePage.jsx
+++ frontend/src/pages/DonatePage.jsx
@@ -16,10 +16,10 @@ const DonatePage = () => {
   const [isRedboxModalOpen, setIsRedboxModalOpen] = useState(false);
   const [donationType, setDonationType] = useState("");
 
-  const handleSubmit = async (quantity, comment, receivedId = null) => {
+  const handleSubmit = async (quantity, comment, receiveId = null) => {
     try {
       await api.post(`/donate/${donationType}`, {
-        receivedId,
+        receiveId,
         quantity,
         comment,
       });
