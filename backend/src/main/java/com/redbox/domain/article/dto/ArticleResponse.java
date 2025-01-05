8f739a0179126d61eaf81fa7c2d8613e7e8d781c
leeys9423 <leeys9423@gmail.com>
2025-01-05 13:01:07 +0900

    fix: 헌혈기사 목록 역직렬화 안되는 오류 수정

--- backend/src/main/java/com/redbox/domain/article/dto/ArticleResponse.java
+++ backend/src/main/java/com/redbox/domain/article/dto/ArticleResponse.java
@@ -1,18 +1,25 @@
 package com.redbox.domain.article.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.redbox.domain.article.entity.Article;
 import lombok.Getter;
 
 import java.time.LocalDate;
 
 @Getter
+@JsonDeserialize
 public class ArticleResponse {
 
-    private final Long articleNo;
-    private final String subject;
-    private final String url;
-    private final String source;
-    private final LocalDate createdDate;
+    private Long articleNo;
+    private String subject;
+    private String url;
+    private String source;
+    private LocalDate createdDate;
+
+    @JsonCreator
+    public ArticleResponse() {
+    }
 
     public ArticleResponse(Article article) {
         this.articleNo = article.getId();
