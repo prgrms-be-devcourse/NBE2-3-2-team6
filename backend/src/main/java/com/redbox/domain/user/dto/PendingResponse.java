545fcff238cc01b197b5112526803fb59b1a6903
Bin-Choi <csb5268@gmail.com>
2025-01-06 01:51:17 +0900

    fix: 기부 대기 게시글 조회 수정 - PendingResponse 생성 - 쿼리 수정

---
+++ backend/src/main/java/com/redbox/domain/user/dto/PendingResponse.java
@@ -0,0 +1,25 @@
+package com.redbox.domain.user.dto;
+
+import com.redbox.domain.donation.entity.DonationGroup;
+
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class PendingResponse {
+
+    private final int donationAmount;
+    private final LocalDate donationDate;
+    private final String donationMessage;
+    private final Long donationId;
+    private final Long donationGroupId;
+
+    public PendingResponse(DonationGroup donationGroup) {
+        this.donationId = donationGroup.getId();
+        this.donationAmount = donationGroup.getDonationAmount();
+        this.donationDate = donationGroup.getDonationDate();
+        this.donationMessage = donationGroup.getDonationMessage();
+        this.donationGroupId = donationGroup.getId();
+    }
+}
